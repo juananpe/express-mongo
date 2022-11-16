543634d273884f98875469b2f064c6246a6b353f
Juanan Pereira <juanan.pereira@ehu.eus>
2022-11-16 19:23:47 +0100

    add CRUD support

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const mongojs = require('mongojs')
-const db = mongojs('mongodb://127.0.0.1:27017/test', ['inventory'])
+const db = mongojs('mongodb://127.0.0.1:29000/test', ['inventory'])
 const app = express();
 const port = 3000;
+
+// enable json body parsing
+app.use(express.json());
+
+// enable post body parsing
+app.use(express.urlencoded({ extended: true }));
 
 // use templates
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/inventory/delete/:id', (req, res) => {
+    db.inventory.remove({_id: mongojs.ObjectId(req.params.id)}, (err, result) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
 
+app.post('/inventory', (req, res) => {
+    // insert the document
+    db.inventory.insert(req.body, (err, result) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
 
 app.get('/inventory', (req, res) => {
     db.inventory.find((err, docs) => {
@@ -17,6 +41,35 @@ app.get('/inventory', (req, res) => {
             res.send(err);
         } else {
             res.render('inventory', {elements: docs})
+        }
+    })
+})
+
+app.get('/edit/:id', (req, res) => {
+    db.inventory.findOne({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(doc)
+            res.render('edit', {element: doc})
+        }
+    })
+})
+
+app.post('/edit/:id', (req, res) => {
+
+    req.body.size = JSON.parse(req.body.size)
+    console.log(req.body)
+    console.log(req.params.id)
+
+    db.inventory.findAndModify({
+        query: {_id: mongojs.ObjectId(req.params.id)},
+        update: {$set: req.body}},
+        (err, result) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.redirect('/inventory')
         }
     })
 })
