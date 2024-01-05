bb80267c5ca6b1e50338fc25cb0b8017543ccda7
Juanan Pereira <juanan.pereira@ehu.eus>
2024-01-05 18:55:44 +0100

    add post route

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongojs = require('mongojs')
 const db = mongojs('mongodb://127.0.0.1:27017/test', ['inventory'])
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+
+// the middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
 
 // use templates
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.redirect('/inventory'));
 
 
 app.get('/inventory', (req, res) => {
@@ -20,5 +25,22 @@ app.get('/inventory', (req, res) => {
         }
     })
 })
+
+app.post('/inventory/add', (req, res) => {
+    let newItem = {
+        item: req.body.item,
+        qty: parseInt(req.body.qty),
+        size: JSON.parse(req.body.size), // Assuming size is in JSON format
+        status: req.body.status
+    };
+
+    db.inventory.insert(newItem, (err, doc) => {
+        if (err) {
+            res.send("There was a problem adding the information to the database.");
+        } else {
+            res.redirect('/inventory');
+        }
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
